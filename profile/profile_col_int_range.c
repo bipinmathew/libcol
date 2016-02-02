49634173d966085681b439a7a06dec12ae207d56
Bipin Mathew <bipinmathew@gmail.com>
2016-02-02 12:48:35 -0500

    Add benchmark for col_int_sum. Also create a Makefile.

--- profile/profile_col_int_range.c
+++ profile/profile_col_int_range.c
@@ -7,13 +7,14 @@ int main (void){
   unsigned int NUMTRIALS = 10;
 
   clock_t start, diff;
-  double msec;
+  double range_msec, sum_msec;
   unsigned int size;
+  int sum;
 
   size=1000;
 
   for(size=1000;size<1000000000;size*=10){
-    msec = 0;
+    range_msec = 0;
     for(i=0;i<NUMTRIALS;i++){
       if(NO_ERROR!=col_int_init(&a)){
         return(1);
@@ -26,12 +27,25 @@ int main (void){
         }
 
       diff = clock()-start;
-      msec += diff*1000 / CLOCKS_PER_SEC;
+      range_msec += diff*1000 / CLOCKS_PER_SEC;
+
+
+
+      start = clock();
+
+        if(NO_ERROR!=col_int_sum(a,&sum)){
+          return(1);
+        }
+
+      diff = clock()-start;
+      sum_msec += diff*1000 / CLOCKS_PER_SEC;
+
+
 
       col_int_free(a);
     }
 
-    printf("size: %d, avg_time: %f\n",size,msec/NUMTRIALS);
+    printf("size: %d, range_avg_time: %f, sum_avg_time: %f\n",size,range_msec/NUMTRIALS,sum_msec/NUMTRIALS);
   }
 
   return(0);
